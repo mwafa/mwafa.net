5f1311b8c9d43ceb4f4f3cf5968bc113791088e4
Muhammad Wafa <wafax.4@gmail.com>
2020-07-19 03:50:59 +0700

    Add Particle Background

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
-import { Flex, Image, Heading } from "@chakra-ui/core"
+import { Flex, Image, Heading, Box } from "@chakra-ui/core"
 import { SITE } from "../variable/site"
+import Particles from "react-particles-js"
+import { PARTICLES_CONFIG } from "../variable/particles"
 
 export const Header: React.FC = () => {
   return (
@@ -10,21 +12,26 @@ export const Header: React.FC = () => {
       justifyContent="center"
       h="100vh"
       p={5}
-      bg="cyan.50"
+      bg="cyan.300"
     >
-      <Image
-        rounded="full"
-        mb={5}
-        maxW="200px"
-        p={3}
-        src={SITE.profile.avatar}
-      />
-      <Heading py={3} textAlign="center">
-        {SITE.profile.name}
-      </Heading>
-      <Heading size="md" fontWeight="normal">
-        {SITE.profile.desc}
-      </Heading>
+      <Box w="100%" h="100vh" zIndex={0} pos="absolute" top={0}>
+        <Particles params={PARTICLES_CONFIG} height="100vh" />
+      </Box>
+      <Flex alignItems="center" direction="column" zIndex={2}>
+        <Image
+          shadow="8px 5px 20px gray"
+          rounded="full"
+          mb={5}
+          maxW="200px"
+          src={SITE.profile.avatar}
+        />
+        <Heading py={3} textAlign="center">
+          {SITE.profile.name}
+        </Heading>
+        <Heading size="md" fontWeight="normal">
+          {SITE.profile.desc}
+        </Heading>
+      </Flex>
     </Flex>
   )
 }
