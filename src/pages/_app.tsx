d96b0aca22d450791c68fac53f084f5ff65c4dc7
Muhammad Wafa <wafax.4@gmail.com>
2020-07-18 20:11:04 +0700

    update page title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { ThemeProvider, CSSReset, ColorModeProvider } from "@chakra-ui/core"
 import App from "next/app"
+import Head from "next/head"
 import { myTheme } from "../themes/myThemes"
 
 export default class app extends App {
@@ -8,6 +9,9 @@ export default class app extends App {
     const { Component, pageProps } = this.props
     return (
       <ThemeProvider theme={myTheme}>
+        <Head>
+          <title>Muhammad Wafa</title>
+        </Head>
         <ColorModeProvider value="light">
           <CSSReset />
           <Component {...pageProps} />
