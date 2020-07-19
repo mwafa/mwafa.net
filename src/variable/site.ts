a786c206cdf8cca2256624c23130b28904c33172
Muhammad Wafa <wafax.4@gmail.com>
2020-07-19 14:33:36 +0700

    fix: typo

--- src/variable/site.ts
+++ src/variable/site.ts
@@ -3,7 +3,7 @@ export const SITE = {
   profile: {
     name: "Muhammad Wafa",
     avatar: "//github.com/mwafa.png",
-    desc: "I am a delevoper",
+    desc: "I am a developer",
     github: "mwafa",
     twitter: "eiwafa__",
     email: "wafax.4@gmail.com",
