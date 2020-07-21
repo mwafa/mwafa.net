df918ba0bf74892a307cbdd528197846348e9366
Muhammad Wafa <wafax.4@gmail.com>
2020-07-21 12:09:40 +0700

    github small avatar

--- src/services/githubFollowers.ts
+++ src/services/githubFollowers.ts
@@ -1,15 +1,16 @@
-export const getGithubFollowers = async (username: string) => {
+export const getGithubFollowers = async (
+  username: string,
+  avasize: number = 50
+) => {
   const r = await fetch(`https://api.github.com/users/${username}/followers`)
   const result = await r.json()
-  const data: userFollow[] = result.map(
-    ({ login, avatar_url, html_url }: userFollow) => {
-      return {
-        login,
-        avatar_url,
-        html_url,
-      }
+  const data: userFollow[] = result.map(({ login, html_url }: userFollow) => {
+    return {
+      login,
+      avatar_url: `https://github.com/${login}.png?&size=${avasize}`,
+      html_url,
     }
-  )
+  })
   return data
 }
 
